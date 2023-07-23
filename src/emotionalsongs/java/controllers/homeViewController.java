9f6b28232fd046c6ee24678f573a421100fa2000
caskasclass <caskasclass@gmail.com>
2023-07-23 18:23:04 +0200

    UI e cod di homeview

--- src/emotionalsongs/java/controllers/homeViewController.java
+++ src/emotionalsongs/java/controllers/homeViewController.java
@@ -1,17 +1,33 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import util.ColorsManager;
+import util.FXMLLoaders;
 
 public class homeViewController {
 
     @FXML
     private HBox primaryShader;
+
+    @FXML HBox playlistBoxContainer;
+
+    @FXML
+    private FlowPane albumBoxContainer;
+
+    @FXML
+    private TableView<?> tableViewTopClicked;
+    @FXML
+    private HBox othersPlaylistBoxContainer;
+
+    private FXMLLoaders loader = new FXMLLoaders();
 
     @FXML
     private void initialize() {
@@ -22,9 +38,29 @@ public class homeViewController {
             cornerRadii,
             null
         );
-
         Background background = new Background(backgroundFill);
         primaryShader.setBackground(background);
+
+
+        //aggiungo i box per le playlist , per il test ne bastano 4 
+        for (int i = 0; i < 4; i++ ){
+            playlistBoxContainer.getChildren().add((VBox) loader.loadFXML("playlistBox.fxml"));
+        }
+
+
+        //aggiungo i box per gli album , per il test ne bastano 4
+        for (int i = 0; i < 6; i++ ){
+            albumBoxContainer.getChildren().add((HBox) loader.loadFXML("albumBox.fxml"));
+        }
+
+
+        //aggiungo i box per le playlist , per il test ne bastano 4 
+        for (int i = 0; i < 4; i++ ){
+            othersPlaylistBoxContainer.getChildren().add((VBox) loader.loadFXML("playlistBox.fxml"));
+        }
+
+
+
 
     }
 
--- src/emotionalsongs/java/controllers/signWindowController.java
+++ src/emotionalsongs/java/controllers/signWindowController.java
@@ -4,7 +4,6 @@ import util.FXMLLoaders;
 import javafx.scene.layout.Pane;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
