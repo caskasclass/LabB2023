ee314946d26a45105b7dd2724ff73bc6f088d3ee
beabyrice <103578274+beabyrice@users.noreply.github.com>
2023-08-17 16:58:35 +0200

    new stuff

--- src/emotionalsongs/java/controllers/creazionePlaylistController.java
+++ src/emotionalsongs/java/controllers/creazionePlaylistController.java
@@ -9,6 +9,14 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import util.TableViewManager;
+import WindowsLoader.ImagesWindow;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
+import util.ColorsManager;
+import views.HomeView;
+import views.CreatePlaylistView;
+import javafx.stage.Stage;
+import javafx.scene.effect.BoxBlur;
 
 public class creazionePlaylistController {
 
@@ -44,10 +52,37 @@ public class creazionePlaylistController {
     private void initializeElements(double width) {
         TableViewManager playlistTracks = new TableViewManager();
         playlistTracks.setMinHeight(300);
-        tableContainer.getChildren().add(0,playlistTracks);
+        playlistTracks.setVisible(false);
+        //tableContainer.getChildren().add(0,playlistTracks);
         TableViewManager findTracks = new TableViewManager();
         findTracks.setMinHeight(300);
-        tableContainer.getChildren().add(2,findTracks);
+        findTracks.setVisible(false);
+        cerca.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.isEmpty()) {
+                findTracks.setVisible(false); // Hide TableView when the TextField is empty
+            } else {
+                findTracks.setVisible(true); // Show TableView when there is text in the TextField
+            }
+        });
+        tableContainer.getChildren().add(findTracks);
 
     }
+
+    public void openImages(MouseEvent e){
+        Stage parent = (Stage) createButton.getScene().getWindow();
+        ImagesWindow window = new ImagesWindow();
+        window.initOwner(parent);
+        BoxBlur blur = new BoxBlur(10, 10, 3);
+        parent.getScene().getRoot().setEffect(blur);
+        Platform.runLater(() -> {
+            parent.getScene().getRoot().setDisable(true);
+        });
+        window.setOnHidden(event -> {
+            parent.getScene().getRoot().setEffect(null);
+            parent.getScene().getRoot().setDisable(false);
+        });
+        window.show();
+    }
+
+    
 }
