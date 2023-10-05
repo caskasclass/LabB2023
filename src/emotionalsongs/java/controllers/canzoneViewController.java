dd324d3dfc33c60a2c7e5106c969aaab67b0b78f
Filippo <fily20s@gmail.com>
2023-10-05 15:41:33 +0200

    pull

--- src/emotionalsongs/java/controllers/canzoneViewController.java
+++ src/emotionalsongs/java/controllers/canzoneViewController.java
@@ -3,8 +3,13 @@ package controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import tmp.Canzone;
 
 public class canzoneViewController {
+
+
+
+    Canzone c = null;
 
 
     
@@ -12,12 +17,26 @@ public class canzoneViewController {
     private VBox rootCanzoneview;
     @FXML
     private Label nomeCanzone;
+  @FXML
+    private Label nomeAlbum;
+
+
 
 
     @FXML
     void initialized(){
-        nomeCanzone.setText("fireworks");
-        Label l1 = new Label("fireworks");
-        rootCanzoneview.getChildren().add(l1);
+        System.out.println("\n\n\n ciao \n\n\n");
+        nomeCanzone.setText(c.getTrackName());
+        nomeAlbum.setText(c.getTrackAlbum());
+        Label label= new Label("ciao");
+        rootCanzoneview.getChildren().add(label);
+
+
     }
+
+    public void setCanzone(Canzone canzone){
+        c=canzone;
+    }
+
+
 }
