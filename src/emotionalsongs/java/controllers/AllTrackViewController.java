cafd395e0f82f58263e126b52579365d2c02c6ab
barbioo <128720446+barbioo@users.noreply.github.com>
2023-12-22 23:36:28 +0100

    Inserimento AllTrackView

---
+++ src/emotionalsongs/java/controllers/AllTrackViewController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class AllTrackViewController {
+
+    @FXML
+    private Pane MainFrame;
+
+    @FXML
+    private Label LeftLabel;
+
+    @FXML
+    private TextField SearchBar;
+
+    @FXML
+    public void initialize() {
+        
+    }
+}
