f7c981b5df8435bd8272ea7a725028273fd7776b
Filappo <129284696+Filappo@users.noreply.github.com>
2024-01-05 16:12:37 +0100

    da aggiungere commento al package

--- src/emotionalsongs/java/Session/WindowAppearance.java
+++ src/emotionalsongs/java/Session/WindowAppearance.java
@@ -1,7 +1,17 @@
 package Session;
 
 import javafx.stage.Screen;
+/**
+ * Progetto laboratorio B: "Emotional Songs", anno 2022-2023
+ * 
+ * @author Beatrice Bastianello, matricola 751864, VA
+ * @author Lorenzo Barbieri  , matricola 748695, VA
+ * @author Filippo Storti , matricola 749195, VA
+ * @author Nazar Viytyuk, matricola 748964, VA
+ * @version 1.0
 
+ *classe creata per gestire le dimensioni e la posizione di una finestra in base a determinati parametri
+ */
 public class WindowAppearance {
 
     private static final double WIDTH_PERCENTAGE = 0.85;
@@ -14,6 +24,8 @@ public class WindowAppearance {
     private static double windowX;
     private static double windowY;
 
+
+/*Metodo per calcolare le dimensioni e la posizione della finestra in base alle dimensioni dello schermo e alle costanti di percentuali*/
     public static void calculateWindowDimensions() {
         screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
         screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
@@ -25,6 +37,10 @@ public class WindowAppearance {
         windowY = (screenHeight - windowHeight) / 2;
     }
 
+
+    
+/** Metodi pubblici Getter che restituiscono le dimensioni e a posizione della finestra
+*/
     public static double getWindowWidth() {
         return windowWidth;
     }
