1926748d9963f9a53d5950d0897feeedf27f7060
Filappo <129284696+Filappo@users.noreply.github.com>
2024-01-05 16:43:38 +0100

    da aggiungere commento al package

--- src/emotionalsongs/java/Threads/ResizeHandler.java
+++ src/emotionalsongs/java/Threads/ResizeHandler.java
@@ -5,6 +5,18 @@ import javafx.application.Platform;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
+/**
+ * Progetto laboratorio B: "Emotional Songs", anno 2022-2023
+ * 
+ * @author Beatrice Bastianello, matricola 751864, VA
+ * @author Lorenzo Barbieri  , matricola 748695, VA
+ * @author Filippo Storti , matricola 749195, VA
+ * @author Nazar Viytyuk, matricola 748964, VA
+ * @version 1.0
+
+ *classe creata per gestire, grazie al thread ResizeHandler, il ridimensionamento della finestra di menu
+ *il thread ascolta i cambiamenti nella larghezza della finestra e, quando rileva una modifica, esegue l'animazione aggiornando la larghezza del menu in base alla nuova larghezza della finestra
+ */
 public class ResizeHandler extends Thread {
 
     public static  double MaxWidth = WindowAppearance.getWindowWidth() * 0.25; // non toccare
@@ -13,15 +25,25 @@ public class ResizeHandler extends Thread {
     private VBox rootMenu;
 
 
+/**Costruttore che accetta il BorderPane principale e la VBox del menu*/
     public ResizeHandler(BorderPane rootPane, VBox rootMenu) {
         this.setDaemon(true);
         this.rootPane = rootPane;
         this.rootMenu = rootMenu;
     }
 
+
+
+/** Metodo synchronized per ottenere la larghezza corrente del centro
+*@return <code>currentCenterRegisteredWidth</code> istanza che riproduce la larghezza corrente del centro
+*/
     public static synchronized double getCenterWidth(){
         return currentCenterRegisteredWidth;
     }
+
+
+
+/** Metodo che gestisce l'animazione del ridimensionamento*/
     public void run() {
         rootPane.widthProperty().addListener((obs, oldWidth, newWidth) -> {
             Platform.runLater(() -> {
@@ -31,6 +53,12 @@ public class ResizeHandler extends Thread {
         });
 
     }
+
+
+
+/**Metodo per animare la larghezza del menu in base alla nuova larghezza della finestra
+*@param <code>newWidth</code> variabile di tipo double
+*/
     private void animateMenuWidth(double newWidth) {
         double menuWidth = Math.min(newWidth * 0.23, MaxWidth); // Calcolo della nuova larghezza
         rootMenu.setPrefWidth(menuWidth);
