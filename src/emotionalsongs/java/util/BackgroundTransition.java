f5d2e7d7dd82c18ba509a3327b79133f116d29ef
Filappo <129284696+Filappo@users.noreply.github.com>
2024-01-06 16:14:06 +0100

    bea guardalo x dare ok definitivo

--- src/emotionalsongs/java/util/BackgroundTransition.java
+++ src/emotionalsongs/java/util/BackgroundTransition.java
@@ -17,7 +17,18 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Stop;
+/**
+ * Progetto laboratorio B: "Emotional Songs", anno 2022-2023
+ * 
+ * @author Beatrice Bastianello, matricola 751864
+ * @author Barbieri Lorenzo , matricola 748695
+ * @author Storti Filippo , matricola 749195
+ * @author Nazar Viytyuk, matricola 748964
+ * @version 1.0
 
+ *classe creata estendendo la classe Transition
+ *funzionalità di gestione delle transizioni di sfondo di un oggetto di tipo HBox (un contenitore orizzontale di JavaFX) da un colore a un altro durante un intervallo di tempo specificato
+ */
 public class BackgroundTransition extends Transition {
     public static  Color hbox_header;
     private static final Color shader = Color.rgb(18, 18, 18);
@@ -25,10 +36,15 @@ public class BackgroundTransition extends Transition {
     private final  Color initialBackground;
     private final  Color targetBackground;
 
+
+ /**metodo statico che imposta il colore di sfondo */
     public static void setHeaderGraphics(Color header){
         hbox_header= header;
     }
 
+
+ 
+/** costruttore della classe: viene fornita la HBox, la durata della transizione, il colore iniziale e il colore finale*/
     public BackgroundTransition(HBox hbox,Duration duration,Color oldColor, Color newColor){
         hbox_header = newColor;
         this.hBox = hbox;
@@ -37,6 +53,12 @@ public class BackgroundTransition extends Transition {
         setCycleDuration(duration);
     }
 
+
+ 
+ /**metodo che viene chiamato durante l'animazione
+ *Calcola il colore di sfondo interpolato in base al valore di frac e imposta il nuovo sfondo della HBox
+*@param <code>frac</code> parametro di tipo double
+*/   
     @Override
     protected void interpolate(double frac){
         Background transition = getGradientBackground(frac);
@@ -44,6 +66,11 @@ public class BackgroundTransition extends Transition {
         
     }
 
+
+ 
+ /**metodo statico che restituisce uno sfondo con una sfumatura lineare da color a un colore predefinito
+*@param <code>color</code> parametro di tipo Color
+*/   
     public static Background gettLinearGradient(Color color){
         CornerRadii cornerRadii = new CornerRadii(8, 8, 0, 0, false);
         LinearGradient linearGradient = new LinearGradient(0, 0, 0, 1, true, CycleMethod.NO_CYCLE, new Stop[] {
@@ -54,6 +81,12 @@ public class BackgroundTransition extends Transition {
         return (new Background(backgroundFill));
         
     }
+
+
+
+ /**metodo privato che restituisce uno sfondo con una sfumatura lineare da initialBackground a targetBackground in base al valore di frac
+*@param <code>frac</code> parametro di tipo double
+*/  
     private  Background getGradientBackground(double frac) {
         CornerRadii cornerRadii = new CornerRadii(8, 8, 0, 0, false);
         Stop[] stops = new Stop[] {
